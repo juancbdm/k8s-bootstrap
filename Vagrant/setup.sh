62162eac036f4b553eab29d04930ccda86d1a46e
Juan Camargo <juancbdm@gmail.com>
2021-11-16 23:49:12 -0300

    * Add a nice banner for the sctript * Unwrap non installation packages commands * Add suport for Ubuntu

--- Vagrant/setup.sh
+++ Vagrant/setup.sh
@@ -2,6 +2,15 @@
 set -o errtrace
 
 main () {
+
+echo "
+██╗  ██╗ █████╗ ███████╗     ██████╗ ███╗   ██╗    ██╗   ██╗ █████╗  ██████╗ ██████╗  █████╗ ███╗   ██╗████████╗
+██║ ██╔╝██╔══██╗██╔════╝    ██╔═══██╗████╗  ██║    ██║   ██║██╔══██╗██╔════╝ ██╔══██╗██╔══██╗████╗  ██║╚══██╔══╝
+█████╔╝ ╚█████╔╝███████╗    ██║   ██║██╔██╗ ██║    ██║   ██║███████║██║  ███╗██████╔╝███████║██╔██╗ ██║   ██║   
+██╔═██╗ ██╔══██╗╚════██║    ██║   ██║██║╚██╗██║    ╚██╗ ██╔╝██╔══██║██║   ██║██╔══██╗██╔══██║██║╚██╗██║   ██║   
+██║  ██╗╚█████╔╝███████║    ╚██████╔╝██║ ╚████║     ╚████╔╝ ██║  ██║╚██████╔╝██║  ██║██║  ██║██║ ╚████║   ██║   
+╚═╝  ╚═╝ ╚════╝ ╚══════╝     ╚═════╝ ╚═╝  ╚═══╝      ╚═══╝  ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   
+"
 	[[ $(id -u) -ne 0 ]] && error "This script needs to be run on sudo: sudo ./setup.sh"
 
 	local OS_ENV="Ubuntu"
@@ -15,33 +24,42 @@ main () {
 	if [[ "$OS_ENV" == "Fedora" ]]; then
 		# Check programs
 		echo "Checking if Vagrant and VirtualBox are installed"
-		# shellcheck disable=SC2179
 		[[ -z "$(dnf list installed vagrant | grep  -o 'vagrant' | head -1)" ]] && pkgtoinstall+="vagrant "
-		# shellcheck disable=SC2179
 		[[ -z "$(dnf list installed virtualbox | grep  -o 'VirtualBox' | head -1)" ]] && pkgtoinstall+="VirtualBox-6.1 virtualbox-guest-additions"
 		# Install packages
-		# Sellcheck disable=SC2128	
 		if [[ -n "${pkgtoinstall}" ]]; then
 			echo "Installing packages"
-			dnf install -y gcc binutils make glibc-devel patch libgomp glibc-headers kernel-headers kernel-devel-"$(uname -r)" dkms ${pkgtoinstall}
-			usermod -a -G vboxusers "${SUDO_USER}"
-		
-			# Finish configure VB
-			echo "Setup virtualbox"
-			/bin/bash /usr/lib/virtualbox/vboxdrv.sh setup
-			echo "Install extension pack for virtualbox"
-			udo -u "${SUDO_USER}" bash -c 'url -L -o "Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack" "https://download.virtualbox.org/virtualbox/6.1.28/Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack" && virtualbox Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack'
+			dnf install -y kernel-headers kernel-devel-"$(uname -r)" dkms ${pkgtoinstall}
 		fi
 		echo "All Packages are installed"
-		echo "Install vagrant disk size plugin"
-		sudo -u "${SUDO_USER}" bash -c 'vagrant plugin install vagrant-disksize'
-		echo "Start the vagrantfile"
-		sudo -u "${SUDO_USER}" bash -c 'vagrant up --provider=virtualbox'
+	elif [[ "$OS_ENV" == "Ubuntu" ]]; then
+		# For Debian base distros
+		echo "Installing packages"
+		apt install -y build-essential linux-headers-"$(uname -r)"
+		apt install -y gcc binutils make kernel-headers kernel-devel-"$(uname -r)" dkms
+		if dpkg -S vagrant;then echo "package vagrant is alredy installed"; else apt install -y vagrant; fi 
+		if dpkg -S virtualbox;then echo "package virtualbox is alredy installed"; else apt install -y virtualbox virtualbox-ext-pack; fi 
+		echo "All Packages are installed"
 	else
-		echo "Ubuntu"
+		error "Not match distros"
 	fi
-	
-	echo "Everithing seems fine, enjoy your Kubernetes cluster on you own PC!"
+
+	# Setup for non-root commands
+	usermod -a -G vboxusers "${SUDO_USER}"
+
+	echo "Install vagrant disk size plugin"
+	sudo -u "${SUDO_USER}" bash -c 'vagrant plugin install vagrant-disksize'
+
+	echo "Start the vagrantfile"
+	sudo -u "${SUDO_USER}" bash -c 'vagrant up --provider=virtualbox'
+
+	# Finish configure VB
+	echo "Setup virtualbox"
+	/bin/bash /usr/lib/virtualbox/vboxdrv.sh setup
+	echo "Install extension pack for virtualbox"
+	sudo -u "${SUDO_USER}" bash -c 'url -L -o "Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack" "https://download.virtualbox.org/virtualbox/6.1.28/Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack" && virtualbox Oracle_VM_VirtualBox_Extension_Pack-6.1.28.vbox-extpack'
+
+	echo "Everithing seems fine, enjoy your Kubernetes cluster on Vagrant!"
 	exit 1
 }
 
